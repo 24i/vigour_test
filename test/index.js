3d4195d98f3d0e3297af4e91132df2d96a8fb00c
Shawn Inder <shawn@vigour.io>
2016-04-11 16:46:24 +0200

    - Added the `node` option (runInNode) - Added tests for the `node` option - Fix the console.log replacement so it doesn't break on non-strings

--- test/index.js
+++ test/index.js
@@ -4,6 +4,34 @@ var path = require('path')
 var test = require('tape')
 var Vtest = require('../')
 
+test('can run simple tape tests in node', function (t) {
+  t.plan(1)
+  var options = {
+    node: true,
+    files: path.join(__dirname, 'files', 'simple.js'),
+    quiet: true
+  }
+  var vtest = new Vtest(options)
+  vtest.start()
+    .then((status) => {
+      t.equal(status, 'pass', 'Tests pass')
+    })
+})
+
+test('can run multiple files in node', function (t) {
+  t.plan(1)
+  var options = {
+    node: true,
+    files: [path.join(__dirname, 'files', 'presimple.js'), path.join(__dirname, 'files', 'simple.js')],
+    quiet: true
+  }
+  var vtest = new Vtest(options)
+  vtest.start()
+    .then((status) => {
+      t.equal(status, 'pass', 'Multiple files')
+    })
+})
+
 test('can run simple tape tests in the browser', function (t) {
   t.plan(1)
   var options = {
